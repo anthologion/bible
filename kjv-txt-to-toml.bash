b3a37fdd718a0fdb798771047876eb68602fe46f
aletheist <aletheist@users.noreply.github.com>
2015-04-25 15:41:58 -0400

    Updated txt-to-toml script. Most books should be well formed now.

--- kjv-txt-to-toml.bash
+++ kjv-txt-to-toml.bash
@@ -6,9 +6,12 @@ do
 # 2) Creates the chapter array markers
 # 3) Removes extraneous whitespaces
 # 4) Removes bracketed words
-cat $f | perl -0777pe 's/\r\n/\n/g' \
+cat $f \
+| perl -0777pe 's/\r\n/\n/g' \
 | perl -pe 's/(\{(\d)+:1\}) /"""\n\n[[Chapter]] #$2\ntext="""\\\n$1 /' \
 | perl -pe 's/[ \t][ \t]+/ /' \
 | perl -0777pe 's/([\s,.])\[([:,.;\w\s]+)\]([\s,.])/$1$2$3/g' \
-> `basename ${f%.*}`.toml
+| perl -0777pe 's/\s([\w ]+)\s+"""/[Book]\ntitle=$1/sg' \
+| perl -0777pe 's/\n """/"""/sg' \
+> ./toml/`basename ${f%.*}`.toml
 done
