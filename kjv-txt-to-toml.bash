54131573d98056dbbd1d81736b15d1482952199d
aletheist <aletheist@users.noreply.github.com>
2015-04-25 17:40:28 -0400

    Reformatted the Psalms some. Removed 'Book' divisions of the psalms, and removed their 'verse 0' notes.

--- kjv-txt-to-toml.bash
+++ kjv-txt-to-toml.bash
@@ -6,12 +6,17 @@ do
 # 2) Creates the chapter array markers
 # 3) Removes extraneous whitespaces
 # 4) Removes bracketed words
+
+# With more tweaking, this might be able to recover Psalm notes. For right now
+# I'm not going to worry about that.
+#| perl -0777pe 's/Psalm (\d+)\s+(\w?[\w\s]*)\s+/{$1:0}$2/g' \
 cat $f \
 | perl -0777pe 's/\r\n/\n/g' \
-| perl -pe 's/(\{(\d+):1\}) /"""\n\n[[Chapter]] #$2\ntext="""\\\n$1 /' \
+| perl -0777pe 's/Psalm (\d+)\s+(\w?[\w\s,.;\[\]]*)//g' \
+| perl -pe 's/(\{(\d+):[01]\}) /"""\n\n[[Chapter]] #$2\ntext="""\\\n$1 /' \
 | perl -pe 's/[ \t][ \t]+/ /' \
 | perl -0777pe 's/([\s,.])\[([:,.;\w\s]+)\]([\s,.])/$1$2$3/g' \
 | perl -0777pe 's/\s([\w ]+)\s+"""/[Book]\ntitle="$1"/sg' \
-| perl -0777pe 's/\n """/"""/sg' \
+| perl -0777pe 's/[\s\n]+"""/\n"""/sg' \
 > ./toml/`basename ${f%.*}`.toml
 done
