6fdf824233b96e88b8113ae15c35ceb92d0fb766
aletheist <aletheist@users.noreply.github.com>
2015-04-26 17:36:03 -0400

    Changed script to conform to the new serialization format.

--- kjv-txt-to-toml.bash
+++ kjv-txt-to-toml.bash
@@ -2,21 +2,27 @@
 for f in ./txt/*.txt
 do
 # This bit does several things.
-# 1) Converts Windows newlines to Unix style
-# 2) Creates the chapter array markers
-# 3) Removes extraneous whitespaces
-# 4) Removes bracketed words
+# * Converts Windows newlines to Unix style
+# * Removes extraneous whitespaces
+# * Special Psalm sauce
+# * Creates the chapter array markers
+# * Creates verse entries
+# * Unwraps bracketed words
+# * Adds book marker and title
+# * Tidies up the array ending markers
 
 # With more tweaking, this might be able to recover Psalm notes. For right now
 # I'm not going to worry about that.
 #| perl -0777pe 's/Psalm (\d+)\s+(\w?[\w\s]*)\s+/{$1:0}$2/g' \
 cat $f \
-| perl -0777pe 's/\r\n/\n/g' \
+| perl -0777pe 's/\r\n/ /g' \
+| perl -0777pe 's/[ \t][ \t]+/ /g' \
 | perl -0777pe 's/Psalm (\d+)\s+(\w?[\w\s,.;\[\]]*)//g' \
-| perl -pe 's/(\{(\d+):[01]\}) /"""\n\n[[Chapter]] #$2\ntext="""\\\n$1 /' \
-| perl -pe 's/[ \t][ \t]+/ /' \
+| perl -0777pe 's/(\{(\d+):[01]\}) /\]\n\n[[Chapter]] #$2\nverses=\[\n$1 /g' \
+| perl -0777pe 's/[\s\n]+\]/\n\]/sg' \
+| perl -0777pe '$sq="\047"; s/({\d+:\d+})([\w\s,;."():\[\]$sq]+)(?={\d+:\d+}|\n])/"$2", #$1\n/g' \
 | perl -0777pe 's/([\s,.])\[([:,.;\w\s]+)\]([\s,.])/$1$2$3/g' \
-| perl -0777pe 's/\s([\w ]+)\s+"""/[Book]\ntitle="$1"/sg' \
-| perl -0777pe 's/[\s\n]+"""/\n"""/sg' \
+| perl -0777pe 's/\s([\w ]+)\s+]/[Book]\ntitle="$1"/sg' \
+| perl -0777pe 's/[\s\n]+\]/\n\]/sg' \
 > ./toml/`basename ${f%.*}`.toml
 done
